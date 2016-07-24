f25b08353d30ac8d0aedc4ff752c7729a1ed22c6
guoguoya <yujiabin0006@gmail.com>
2016-07-24 13:15:43 +0800

    con center

--- control/controlCenter.js
+++ control/controlCenter.js
@@ -1,6 +1,36 @@
+var fs = require('fs');
 var data  = require('./data');
+var path = require('path');
 
 
+function ControlCenter () {
+    this.array = [] ;
+} 
 
+ControlCenter.prototype = {
+    getAll: function(){
+        getdir(__dirname);
+    },
+    getdir: function(filePath){
+        var that = this; 
+        fs.readdir(filePath, function(err,files){
+            if(err){
+                console.error(err);
+            }
+            else {
+                files.forEach( fucntion(file){
+                    var newPath = path.join(filePath, file);
+                    if( fs.stat(newPath).isDirectory() ){
+                        that.getdir(newPath);
+                    }
+                    else{
+                        that.push(file);
+                    }
+                });
+            }
+        });
+    }
+}
+var controlCenter = new ControlCenter();
 
-module.exports = data;
+module.exports = controlCenter;
